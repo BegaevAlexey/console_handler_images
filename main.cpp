cc830d8906972e3082ac03705b2162e74a7f2e59
incoel <project@incoel.ru>
2020-05-26 22:31:29 +0300

    add the handler to main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <memory>
+
+#include "handlerimage/handlerimages.h"
 
 int main(int argc, char **argv)
 {
+    // init handler images
+    std::unique_ptr<hi::HandlerImages> handler = std::make_unique<hi::HandlerImages>();
 
+    while(true)
+    {
+        // read console massage
+        std::string msg;
+        std::getline(std::cin, msg);
 
+        handler->process(msg);
+        if(handler->isWorked())
+        {
+            break;
+        }
+    }
 
     std::cout << "\n\t***END PROGRAM***\n";
     return 0;
