021556733fa241449b7adab5ce3186134d4dddd6
incoel <project@incoel.ru>
2020-05-26 22:46:26 +0300

    add log defines

---
+++ src/utils/defines.h
@@ -0,0 +1,10 @@
+#ifndef CONSOLE_HANDLER_IMAGES_DEFINES_H
+#define CONSOLE_HANDLER_IMAGES_DEFINES_H
+
+#define LOG_INFO(msg) std::cout << "\033[1;32m[INFO]\033[0m " << msg << std::endl
+
+#define LOG_WARNING(msg) std::cout << "\033[1;33m[WARNING]\033[0m " << msg << std::endl
+
+#define LOG_ERROR(msg) std::cout << "\033[1;31m[ERROR]\033[0m " << msg << std::endl
+
+#endif // CONSOLE_HANDLER_IMAGES_DEFINES_H
