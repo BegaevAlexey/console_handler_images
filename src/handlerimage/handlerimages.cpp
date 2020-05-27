2a17f6c95b030ccf529807f5a3e13d714151be5a
incoel <project@incoel.ru>
2020-05-27 22:34:59 +0300

    add getOperations method

--- src/handlerimage/handlerimages.cpp
+++ src/handlerimage/handlerimages.cpp
@@ -14,12 +14,13 @@ hi::HandlerImages::~HandlerImages()
 void hi::HandlerImages::process(const std::string& msg)
 {
     // get type of operation with arguments of operation
-    auto opertions = this->getOperations(msg);
+    auto opertions = HandlerImages::getOperations(msg);
 
     // handling operation with arguments
+//    this->handlingOperation(opertions);
 }
 
-bool hi::HandlerImages::isWorked()
+bool hi::HandlerImages::isWorked() const
 {
     return m_isWorked;
 }
@@ -29,10 +30,15 @@ std::pair<hi::HandlerImages::Operation, std::vector<std::string>> hi::HandlerIma
     // split message
     auto strings = HandlerImages::split(msg);
 
-    // get operation
+    if(strings.empty())
+    {
+        return {Operation::OPR_WAR, {"String of arguments is empty"}};
+    }
 
+    // operation type
+    auto operType = HandlerImages::getTypeOperation(strings[0]);
 
-    return std::pair<Operation, std::vector<std::string>>();
+    return {operType, strings};
 }
 
 std::vector<std::string> hi::HandlerImages::split(const std::string &fullStr)
@@ -58,4 +64,39 @@ std::vector<std::string> hi::HandlerImages::split(const std::string &fullStr)
         }
     }
     return splittedString;
+}
+
+hi::HandlerImages::Operation hi::HandlerImages::getTypeOperation(const std::string& strOpr)
+{
+    if((strOpr == "load") || (strOpr == "ld"))
+    {
+        return Operation::OPR_LOAD;
+    }
+
+    if((strOpr == "store") || (strOpr == "s"))
+    {
+        return Operation::OPR_SAVE;
+    }
+
+    if(strOpr == "blur")
+    {
+        return Operation::OPR_BLUR;
+    }
+
+    if(strOpr == "resize")
+    {
+        return Operation::OPR_RSZ;
+    }
+
+    if((strOpr == "help") || (strOpr == "h"))
+    {
+        return Operation::OPR_HELP;
+    }
+
+    if((strOpr == "exit") || (strOpr == "quit")|| (strOpr == "q"))
+    {
+        return Operation::OPR_EXIT;
+    }
+
+    return Operation::OPR_NONE;
 }
--- src/handlerimage/handlerimages.h
+++ src/handlerimage/handlerimages.h
@@ -21,7 +21,14 @@ protected:
      */
     enum class Operation
     {
-        OPR_EXIT,
+        OPR_EXIT, ///< Exit from app
+        OPR_WAR,  ///< Warning message
+        OPR_LOAD, ///< Load new image
+        OPR_SAVE, ///< Save image
+        OPR_BLUR, ///< Blur image
+        OPR_RSZ,  ///< Resize image
+        OPR_HELP, ///< Show help page
+        OPR_NONE, ///< Unknown operation
     };
 
 public:
@@ -45,17 +52,29 @@ public:
      * @brief Check working process
      * @return true - handler is worked
      */
-    bool isWorked();
+    bool isWorked() const;
 
 protected:
     /*!
      *
-     * @param msg
-     * @return
+     * @param msg[in] message from console
+     * @return Operation type with arguments
      */
-    std::pair<Operation, std::vector<std::string>> getOperations(const std::string &msg);
+    static std::pair<Operation, std::vector<std::string>> getOperations(const std::string &msg);
 
+    /*!
+     * @brief Split string by space
+     * @param fullStr[in] string for splitting
+     * @return vector of strings
+     */
     static std::vector<std::string> split(const std::string &fullStr);
+
+    /*!
+     * @brief Get type of operation from string
+     * @param strOpr[in] String name of operation
+     * @return operation type
+     */
+    static Operation getTypeOperation(const std::string &strOpr);
 
 protected:
     bool m_isWorked; ///< Flag for checking work process
