7fae06615790453c554b71ff094bff98020864ba
incoel <project@incoel.ru>
2020-05-27 21:59:57 +0300

    change split function

--- src/handlerimage/handlerimages.cpp
+++ src/handlerimage/handlerimages.cpp
@@ -40,41 +40,22 @@ std::vector<std::string> hi::HandlerImages::split(const std::string &fullStr)
     std::vector<std::string> splittedString;
     int startIndex = 0;
     int endIndex = 0;
-
-    auto delSpase = [](std::string &val){
-        int i = 0;
-        while (std::isspace(val[i]))
-        {
-            i++;
-        }
-        val.erase(0, i);
-
-        i = val.size() - 1;
-        while (std::isspace(val[i]))
-        {
-            i--;
-        }
-        val.erase(i + 1, val.size() - i - 1);
-    };
-
     while( (endIndex = fullStr.find(" ", startIndex)) < fullStr.size() )
     {
         std::string val = fullStr.substr(startIndex, endIndex - startIndex);
-
-        delSpase(val);
-
         if(!val.empty())
         {
             splittedString.push_back(val);
         }
-
         startIndex = endIndex + 1;
     }
     if(startIndex < fullStr.size())
     {
         std::string val = fullStr.substr(startIndex);
-        delSpase(val);
-        splittedString.push_back(val);
+        if(!val.empty())
+        {
+            splittedString.push_back(val);
+        }
     }
     return splittedString;
 }
